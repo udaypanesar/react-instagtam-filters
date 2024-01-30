3082074f626825335435f0c449005a8d59ab9904
udaypanesar <ud.panesar@gmail.com>
2024-01-30 18:01:59 +0530

    Commit App V1

--- src/components/imagePreview.jsx
+++ src/components/imagePreview.jsx
@@ -1,9 +1,48 @@
-const ImagePreview = () => {
-    return(
-        <>
-       
-        </>
-    )
-}
+import { useRef } from "react";
+import { toPng } from "html-to-image";
+import styled from "styled-components";
 
-export default ImagePreview
+const ImagePreview = ({ fileName, imgFilter }) => {
+  const elementRef = useRef(null);
+
+  const onButtonClick = () => {
+    console.log(fileName);
+    toPng(elementRef.current, { cacheBust: false })
+      .then((data) => {
+        const link = document.createElement("a");
+        link.download = "image.png";
+        link.href = data;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <Figure ref={elementRef} className={imgFilter} style={{  }}>
+        <img style={{ width: "100%" }} src={fileName} />
+        <Button onClick={onButtonClick}>{imgFilter}</Button>
+      </Figure>
+    </>
+  );
+};
+
+export default ImagePreview;
+
+// Styled components to apply CSS
+
+const Figure = styled.figure`
+    max-width: 350px;
+    max-height:350px;
+    overflow: hidden;
+    position: relative;
+`
+const Button = styled.button`
+  position: absolute;
+  bottom: 1em;
+  right: 0;
+  padding: 0.25em 1em 0.25em 0.25em;
+  background: #000;
+  color: #fff;
+`;
--- src/components/imageUpload.jsx
+++ src/components/imageUpload.jsx
@@ -1,18 +1,44 @@
 import { useState } from "react";
+import styled from "styled-components";
+import ImagePreview from "./imagePreview";
 
 const ImageUpload = () => {
   const [file, setFile] = useState();
+
+  const imgFilters = ["aden",  "brooklyn", "earlybird", "kelvin", "moon", "nashville", "slumber", "toaster", "walden"];
 
   const onInputChange = (event) => {
     console.log(event.target.files);
     setFile(URL.createObjectURL(event.target.files[0]));
   };
   return (
-    <>
-      <input type="file" name="uImage" onChange={onInputChange} />
-      <img src={file} />
-    </>
+    <Div>
+      <h1 style={{textAlign:"center",color:"coral"}}>Image Filter</h1>
+      <input type="file" style={{marginLeft:"160px"}} onChange={onInputChange} />
+      <Ul>
+        {imgFilters.map((imgFilter) => {
+          return (
+            <>
+              <li style={{listStyle:"none"}}>
+                <ImagePreview key={imgFilter} fileName={file} imgFilter={imgFilter} />
+              </li>
+            </>
+          );
+        })}
+      </Ul>
+    </Div>
   );
 };
 
 export default ImageUpload;
+
+// Styled components to apply CSS
+const Div = styled.div`
+  max-width:1500px;
+  margin:0 auto;
+`
+const Ul = styled.ul`
+  display: flex;
+  flex-wrap:wrap;
+  justify-content:center
+`
